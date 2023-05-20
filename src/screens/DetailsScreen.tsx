5e07e8fedda13851454d9a07057b4dc73c5d0b95
Nidhin Boban <nidhinboban1996@gmail.com>
2023-05-20 16:14:18 +0530

    added details Screen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function DetailsScreen({ route, navigation }:any) {
 
-  const { data } = route.params;
+  const { title ,data } = route.params;
 
   return (
-    <Text>{data}</Text>
+    <View style={{marginTop:5}}>
+    <Text style={[styles.TextContainer, {fontWeight:"700"}]}>{title}</Text>
+    <Text style={styles.TextContainer}>{data}</Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+    TextContainer:{
+      fontSize:20,
+      textAlign:"center",
+      padding:15,
+      },
+})
